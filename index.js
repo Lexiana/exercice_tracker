9f3263f7c1c8b1f04d6c83a018f4282e56788abe
Lexiana <4115619+Lexiana@users.noreply.github.com>
2024-08-02 14:16:40 +0000

    feat: post username

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const { type } = require("express/lib/response");
+const req = require("express/lib/request");
 
 require("dotenv").config();
 
@@ -45,6 +46,24 @@ const Exercise = mongoose.model("Exercise", exerciseSchema);
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
+
+
+// create new user
+app.post('/api/users', async(req, res)=>{
+  const username = req.body.username;
+  try{
+    //check if username already exists
+    let user = await User.findOne({username: username});
+    // if not creat new user
+    if(!user){
+      user = new User({username: username});
+      await user.save();
+    }
+    res.json({username: user.username, _id: user._id});
+  } catch(err){
+    res.json({error: err.message})
+  })
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
