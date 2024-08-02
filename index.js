cab98d191b79bc8092ea5b4610fa03d6876091fa
Lexiana <4115619+Lexiana@users.noreply.github.com>
2024-08-02 13:59:45 +0000

    feat: set-up

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+require("dotenv").config();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(cors());
+app.use(express.static("public"));
+
+// connect database
+mongoose.conect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-
-
-
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
